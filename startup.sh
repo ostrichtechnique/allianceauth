344bb21106f11fcf2094541403e8f3d298ae1ddd
ostrich technique <cynosuralfieldgenerator@gmail.com>
2014-12-05 17:23:55 +0000

    First commit

--- startup.sh
+++ startup.sh
@@ -11,4 +11,5 @@ python manage.py celeryd &
 python manage.py celerybeat &
 
 #python manage.py runserver &
-gunicorn_django --workers=5 --bind 127.0.0.1:8000 --reload True &
+#gunicorn_django --workers=5 --bind 127.0.0.1:8000 &
+/home/auth/infernal-auth/bin/gunicorn_django --workers=3 --bind 127.0.0.1:8000 &
